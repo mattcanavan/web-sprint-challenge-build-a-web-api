8ec6384ca8f852da3b09a916c6988289eec3218f
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-04 09:47:41 -0800

    Server running and connected to Router. Now to build out API.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,25 @@
-// Write your "actions" router here!
+const express = require('express');
+const ActionsFuncs = require('./actions-model');
+
+const router = express.Router();
+
+//// ENDPOINTS
+
+// [GET] /api/actions sends an array of actions (or an empty array) as the body of the response.
+router.get("/")
+
+// [GET] /api/actions/:id sends an action with the given id as the body of the response.
+router.get("/:id")
+
+// [POST] /api/actions sends the newly created action as the body of the response.
+router.post("/")
+
+// [PUT] /api/actions/:id sends the updated action as the body of the response.
+router.put("/:id")
+
+// [DELETE] /api/actions/:id sends no response body.
+router.delete("/:id")
+
+
+//// FIN
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,30 @@
-// Write your "projects" router here!
+const express = require('express');
+const ProjectFuncs = require('./projects-model');
+
+const router = express.Router();
+
+//// ENDPOINTS
+
+// [GET] /api/projects sends an array of projects (or an empty array) as the body of the response.
+router.get("/")
+
+// [GET] /api/projects/:id sends a project with the given id as the body of the response.
+router.get("/:id")
+
+// [POST] /api/projects sends the newly created project as the body of the response.
+router.post("/")
+
+// [PUT] /api/projects sends the updated project as the body of the response.
+router.put("/")
+
+// [DELETE] /api/projects sends no response body.
+router.delete("/")
+
+// [GET] /api/projects/:id/actions sends an array of actions (or an empty array) as the body of the response.
+router.get("/:id/actions")
+
+
+
+
+//// FIN
+module.exports = router;
